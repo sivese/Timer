ee761531774167ec8f8049d7186b56c58a70a869
sivese <driversivese@gmail.com>
2020-10-26 19:44:29 +0900

    add logger library

--- src/core/app.rs
+++ src/core/app.rs
@@ -1,5 +1,32 @@
-struct App{ }
+use gtk::prelude::*;
+
+pub struct App{ }
 
 impl App {
-    
+    fn app_window() -> gtk::Window {
+        let win = gtk::Window::new(gtk::WindowType::Toplevel);
+        
+        //let hb = gtk::HeaderBar::new();
+        //hb.set_title(Some("1 HOUR - Timer"));
+
+        //win.set_icon(icon);
+        win.set_title("1 HOUR - Timer");
+        win.set_position(gtk::WindowPosition::Center);
+        win.set_default_size(400, 600);
+        win.set_resizable(false);
+        
+        win
+    }
+
+    pub fn run() {
+        let win = App::app_window();
+        let start_button = gtk::Button::with_label("Start");
+        
+        start_button.connect_clicked(move |_button|{
+
+        });
+
+        win.add(&start_button);
+        win.show_all();
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -9,32 +9,17 @@ use gtk::prelude::*;
 
 use std::env::args;
 
-fn build_ui(application: &gtk::Application) {
-    let window = gtk::ApplicationWindow::new(application);
-
-    window.set_title("1 HOUR - Timer");
-    window.set_border_width(10);
-    window.set_position(gtk::WindowPosition::Center);
-    window.set_default_size(350, 300);
-
-
-    let button = gtk::Button::with_label("Start Project");
-    let countdown_label = gtk::Label::new(Some("00:00"));
-    let vbox = gtk::Box::new(gtk::Orientation::Vertical, 8);
-
-    button.connect_clicked(move |_btn|{
-        _btn.set_label("Pause");
-        countdown_label.set_text("00:01");
-    });
-
-    vbox.add(&button);
-    vbox.add(&countdown_label);
-    
-    window.add(&vbox);
-    window.show_all();
-}
+use crate::core::app;
 
 fn main() {
+    env_logger::init();
+
+    if let Ok(_) = gtk::init() {
+        app::App::run();
+        gtk::main();
+    }
+
+    /*
     let application =
         gtk::Application::new(Some("com.open.onehour.timer"), Default::default())
             .expect("Initialization failed...");
@@ -44,4 +29,5 @@ fn main() {
     });
 
     application.run(&args().collect::<Vec<_>>());
+    */
 }
